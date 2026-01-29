8bd532c3d32aeb7d24232fdb1e5e918affa44c74
Aryy234 <marcelo-elizalde@hotmail.com>
2026-01-29 18:13:01 -0500

    primero

---
+++ src/client/matriculaClient.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const consultarTodos = () => {
+  const data = axios.get('http://localhost:8081/matricula/api/v1.0/estudiantes')
+  .then(r=> r.data);
+
+  return data;
+}
